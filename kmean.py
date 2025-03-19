a38f4b2c203a3efc3dfe482ff117a2a7b95427fb
jeeyongl <127641213+jeeyongl@users.noreply.github.com>
2025-03-19 14:51:42 +0000

    2025.3.19

--- kmean.py
+++ kmean.py
@@ -1,17 +1,20 @@
 from __future__ import annotations
 import numpy
-from typing import Iterable, Generic
+from typing import Iterable, Generic, TypeVar
+from dataclasses import dataclass
 import sys
+import random
+from functools import reduce
 
 
 class DataPoint:
     def __init__(self, data: Iterable[float]):
         self._raw_data = numpy.array(data, dtype= numpy.float32).flatten()
-        self.data = self._zscore()
+        self.data = numpy.array(data, dtype= numpy.float32).flatten() #self._zscore()
         self.shape = self.data.shape
 
     def __repr__(self):
-        return f'data: {self.shape}:\n{self.data}' 
+        return f'DataPoint: shape({self.shape}): {self.data}' 
     
     def __eq__(self, other: DataPoint):
         if not isinstance(other, DataPoint) or self.shape != other.shape:
@@ -24,11 +27,11 @@ class DataPoint:
             return numpy.inf
         return numpy.linalg.norm(self.data - other.data)
     
-    def _zscore(self):
-        if numpy.std(self._raw_data) == 0: # all data points are same
-            return numpy.zeros(self._raw_data.shape)
-        else:
-            return (self._raw_data - numpy.mean(self._raw_data) / numpy.std(self._raw_data))
+    # def _zscore(self):
+    #     if numpy.std(self._raw_data) == 0: # all data points are same
+    #         return numpy.zeros(self._raw_data.shape)
+    #     else:
+    #         return (self._raw_data - numpy.mean(self._raw_data) / numpy.std(self._raw_data))
         
 
     
@@ -41,6 +44,75 @@ class DataPoint:
 # 5. recalculate centeroids with new cluster data points
 # 6. repeat 4 and 5
 # 7. stop with maximum iterations or centeroids no move
+P = TypeVar('P', bound= DataPoint)
+class KMeans(Generic[P]):
+    @dataclass
+    class Cluster:
+        centeroid: P|None
+        cluster: list[P]
 
-class KMeans(Generic[DataPoint]):
-    pass
+    def __init__(self, k:int, data_points: list(P)):
+        if k < 1:
+            raise ValueError('k must be positive integer')
+        self.k= k
+        self.data_points = data_points
+        self.zscore_normalize()
+        self.clusters: list(self.Cluster) = []
+        for _ in range(self.k):
+            self.clusters.append(self.Cluster(self.random_centeroids(), []))
+
+    def __repr__(self) -> str:
+        return f'{self.clusters}'
+    
+    def random_centeroids(self) -> P:
+        data= reduce(lambda x,y: numpy.vstack([x.data, y.data]), self.data_points)
+        low, high = (numpy.min(data, axis= 0), numpy.max(data, axis= 0))
+        cent= numpy.zeros(low.shape[0])
+        for i in range(low.shape[0]):
+            cent[i] = random.uniform(low[i], high[i])
+        return DataPoint(cent)
+    
+    def zscore_normalize(self):
+        # combine DataPoints.data
+        data= reduce(lambda x,y: numpy.vstack([x.data, y.data]), self.data_points)
+        # calc z-score
+        for i in range(data.shape[1]):
+            if numpy.std(data[:,i]) == 0: # all data points are same
+                data[:,i] = numpy.zeros(data.shape[0])
+            else:
+                data[:,i] = (data[:,i] - numpy.mean(data[:,i]) / numpy.std(data[:,i]))
+        # put back to DataPoints
+        for i in range(data.shape[0]):
+            self.data_points[i].data = data[i,:]
+        
+    def assign_clusters(self):
+        centeroids= [clust.centeroid.data for clust in self.clusters]
+        for point in self.data_points:
+            dist = numpy.linalg.norm(centeroids - point.data, axis = 1)
+            idx = numpy.argmin(dist)
+            self.clusters[idx].cluster.append(point)
+
+    def calculate_centeroids(self):
+        for clust in self.clusters:
+            data= reduce(lambda x,y: numpy.vstack([x.data, y.data]), clust.cluster)
+            clust.centeroid = DataPoint(data.mean(axis= 0))
+
+    def run(self, max_iterations: int = 100) -> List[KMeans.Cluster]:
+        for iters in range(max_iterations):
+            for clust in self.clusters:
+                clust.cluster.clear()
+
+            self.assign_clusters()
+            previous_centeroids: List[Point] = deepcopy(self._centeroids)
+            self.calculate_centeroids()
+
+            centroid_shift = sum([previous_centeroid.distance(current_centeroid)
+                           for previous_centeroid, current_centeroid in zip(previous_centeroids, self._centeroids)])
+            if centroid_shift < 1e-18:
+                print(f'Converged after {iters} iterations')
+                return self._clusters
+
+        print('Did not converged in 100 iterations')
+        return self._clusters
+        pass
+
--- test_kmean.py
+++ test_kmean.py
@@ -1,4 +1,7 @@
 from kmean import *
+from subprocess import getoutput
+getoutput('python3 -m pip install icecream')
+from icecream import ic
 
 a= DataPoint([[1,2,3],[2,3,4]])
 b= DataPoint([[1,2,3],[2,3,4]])
@@ -6,12 +9,19 @@ c= DataPoint([[1,2,3],[3,3,4]])
 d= DataPoint([[1,2,3,3],[3,3,4,5]])
 e= DataPoint([[1,1,1,1],[1,1,1,1]])
 
-print(a==b)
-print(a==c)
-print(a.distance(c))
-print(a.distance(d))
-print(a)
-print(e)
-print(e.distance(d))
+# ic(a==b)
+# ic(a==c)
+# ic(a.distance(c))
+# ic(a.distance(d))
+# ic(a)
+# ic(e)
+# ic(e.distance(d))
 
-KMeans()
+k= KMeans(2,[a,b,c])
+ic(k)
+ic(k.random_centeroids())
+ic(k.random_centeroids())
+ic(k.assign_clusters())
+ic(k.clusters[0])
+ic(k.clusters[1])
+ic(k.run())
