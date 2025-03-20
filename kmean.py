1bd3036f9ef94454e8db3935f8488ea3da5485d5
jeeyongl <127641213+jeeyongl@users.noreply.github.com>
2025-03-20 09:03:58 +0900

    Update kmean.py

--- kmean.py
+++ kmean.py
@@ -8,6 +8,10 @@ from functools import reduce
 
 
 class DataPoint:
+    self._raw_data: numpy.array
+    self.data: numpy.array
+    self.shape: tuple[int,...]
+    
     def __init__(self, data: Iterable[float]):
         self._raw_data = numpy.array(data, dtype= numpy.float32).flatten()
         self.data = numpy.array(data, dtype= numpy.float32).flatten() #self._zscore()
